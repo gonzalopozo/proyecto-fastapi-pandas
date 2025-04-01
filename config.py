4fbfa0a99d9888de3688f9a032a49e84fda59c5c
gonzalopozo <gonzalopozo2005@gmail.com>
2025-04-01 17:46:22 +0200

    Cambio en el uso de las variables de entorno para el deploy en Linux

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
-
     DRIVER: str
     DSN: str
     HOST: str
