7c914dd427cf566be51bef14a5970b902298271b
gonzalopozo <gonzalopozo2005@gmail.com>
2025-04-01 16:45:14 +0200

    Creación endpoints albaranes por día

--- main.py
+++ main.py
@@ -2,17 +2,35 @@ import pyodbc
 import pandas as pd
 
 # from typing import Annotated
-# from functools import lru_cache
+from functools import lru_cache
 from fastapi import Depends, FastAPI, HTTPException
 from contextlib import contextmanager
 
 from config import Settings
 
-# @lru_cache
+from datetime import date
+
+tags_metadata = [
+    # {
+    #     "name": "Describir tabla",
+    #     "description": "Se obtienen las **columnas** de la tablas indicada",
+    # },
+    {
+        "name": "Albaranes por fecha completa",
+        "description": "Se obtienen los albarenes de ese día usando un **unico parametro**, que es la fecha entera de ese día con el formato **YYYYMMDD** sin espacios",
+    },
+    {
+        "name": "Albaranes por día, mes y año",
+        "description": "Se obtienen los albarenes de ese día usando **tres parametros diferentes**: el día, el mes y el año",
+    },
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
+
+@lru_cache
 def get_settings():
     return Settings()
 
-app = FastAPI()
 
 @contextmanager
 def get_db_connection():
@@ -40,6 +58,23 @@ def get_db_connection():
         if conn:
             conn.close()
 
+# @app.get("/describe_table/{table_name}", tags=["Describir tabla"])
+# def describe_table(table_name: str):
+#     try:
+#         with get_db_connection() as conn:
+#             # query = "DESCRIBE " + table_name
+#             # query = f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = {table_name}"
+#             # query = f"SELECT f._Field-Name AS column_name, f._Data-Type AS data_type FROM PUB._Field f JOIN PUB._File t  f._File-recid = t._recid WHERE t._File-Name = {table_name};"
+#             # query = f"SELECT c.colname AS column_name, c.coltype AS data_type FROM sysprogress.syscolumns c JOIN sysprogress.systables t ON c.tabid = t.tabid WHERE t.tablename = {table_name};"
+#             query = f"SELECT * FROM DICTDB._FIELD WHERE _FILE-RECID = (SELECT _RECID FROM DICTDB._FILE WHERE _FILE-NAME = {table_name})"
+
+#             df = pd.read_sql_query(query, conn)  # Datos obtenidos se devuelven como un objeto de tipo DataFrame
+
+#             return {"message": f"Table {table_name} described correctly", "results": df.to_dict(orient="records")}
+#     except Exception as e:
+#         raise HTTPException(status_code = 500, detail = str(e))
+
+
 @app.get("/test")
 def test_api():
     try:
@@ -58,7 +93,42 @@ def get_cabeceras_albaranes():
         with get_db_connection() as conn:
             query = "SELECT cod_cli, doc_alb, fec_alb, raz_cli, tot_alb FROM pub.gvalcab"
             df = pd.read_sql_query(query, conn)
-            df = df.head(100)
+            df = df.tail(100)
+            df.loc[df['doc_alb'] == 'A', 'tot_alb'] *= -1
+
+            return {"status": "success", "message": "Request processed successfully",  "results": df.to_dict(orient="records")}
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail = str(e))
+
+@app.get("/api/albaranes/cabeceras/{day}", tags=["Albaranes por fecha completa"])
+def get_cabeceras_albaranes(day: str):
+    try:
+        with get_db_connection() as conn:
+
+            processed_date = date(int(day[:4]), int(day[4:6]), int(day[6:8]))
+
+            # query = f"SELECT cod_cli, doc_alb, fec_alb, raz_cli, tot_alb FROM pub.gvalcab WHERE fec_alb = {processed_date.isoformat()}"
+            query = f"SELECT cod_cli, doc_alb, fec_alb, raz_cli, tot_alb FROM pub.gvalcab WHERE fec_alb = '{processed_date.isoformat()}'"
+
+            print(query)
+            df = pd.read_sql_query(query, conn)
+            df.loc[df['doc_alb'] == 'A', 'tot_alb'] *= -1
+
+            return {"status": "success", "message": "Request processed successfully",  "results": df.to_dict(orient="records")}
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail = str(e))
+
+@app.get("/api/albaranes/cabeceras/{year}/{month}/{day}", tags=["Albaranes por día, mes y año"])
+def get_cabeceras_albaranes(year: str, month: str, day: str):
+    try:
+        with get_db_connection() as conn:
+
+            processed_date = date(int(year), int(month), int(day))
+
+            query = f"SELECT cod_cli, doc_alb, fec_alb, raz_cli, tot_alb FROM pub.gvalcab WHERE fec_alb = '{processed_date.isoformat()}'"
+
+            print(query)
+            df = pd.read_sql_query(query, conn)
             df.loc[df['doc_alb'] == 'A', 'tot_alb'] *= -1
 
             return {"status": "success", "message": "Request processed successfully",  "results": df.to_dict(orient="records")}
